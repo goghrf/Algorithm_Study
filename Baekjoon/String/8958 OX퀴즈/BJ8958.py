476132ba84e96d8d8ebb0bb5386f1fa046a91b81
goghrf <daynnite9924@gmail.com>
2023-03-28 23:53:54 +0900

    문제 주석 추가

--- Baekjoon/String/8958 OX퀴즈/BJ8958.py
+++ Baekjoon/String/8958 OX퀴즈/BJ8958.py
@@ -3,14 +3,17 @@ input = sys.stdin.readline
 
 n = int(input())
 
+# n만큼 반복
 for _ in range(n):
     count = 0
     total = 0
     quiz = input()
     for i in quiz:
+        # 0일 때
         if i == "O":
             count += 1
             total += count
+        # X일 때
         else:
             count = 0
             total += count
