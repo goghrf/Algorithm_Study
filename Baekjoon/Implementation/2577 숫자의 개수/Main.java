5f459a75f79e07d0c2be05e4e774ba01972d8572
goghrf <daynnite9924@gmail.com>
2023-04-20 22:08:09 +0900

    문제 추가

---
+++ Baekjoon/Implementation/2577 숫자의 개수/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        int num = A*B*C;
+
+        int[] arr = new int[10];
+
+        while(num>0){
+            int k = num%10;
+            arr[k]++;
+            num = num/10;
+        }
+
+        for(int i = 0; i<10; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
